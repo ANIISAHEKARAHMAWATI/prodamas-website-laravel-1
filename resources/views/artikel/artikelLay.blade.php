78bbebec7bfa7b80ca737be5b7bbde13f710c5b7
Diyah Anggraini <anggrainidiyah25@gmail.com>
2022-01-18 13:58:17 +0700

    sampul responsive

--- resources/views/artikel/artikelLay.blade.php
+++ resources/views/artikel/artikelLay.blade.php
@@ -75,6 +75,18 @@
         </div>
     </div>
 <!-- Akhir content -->
+
+    <style>
+    /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 800px) {
+  .leftcolumn, .leftcolumn img, .rightcolumn{   
+    width: 100%;
+    padding: 0;
+    margin-left: 0;
+  }
+}
+    </style>
+
 </body>
 </html>
 @endsection
