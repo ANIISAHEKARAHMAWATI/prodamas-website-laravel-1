0db48b794af478bbd4f86a8bf71338fad8205ebb
Diyah Anggraini <anggrainidiyah25@gmail.com>
2022-01-18 15:49:48 +0700

    info lainnya

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -93,7 +93,7 @@
       </a>
       @endforeach
     </div>
-    <a href="{{ url('bincangprodamas') }}">
+    <a href="{{ url('artikel') }}">
       <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
           </svg></u></h6>
@@ -133,7 +133,7 @@
         </div>
       </div>
     </div>
-    <a href="{{ url('bincangprodamas') }}">
+    <a href="{{ url('video') }}">
       <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
           </svg></u></h6>
@@ -173,7 +173,7 @@
         </div>
       </div>
     </div>
-    <a href="{{ url('bincangprodamas') }}">
+    <a href="{{ url('foto') }}">
       <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
           </svg></u></h6>
@@ -213,7 +213,7 @@
         </div>
       </div>
     </div>
-    <a href="{{ url('bincangprodamas') }}">
+    <a href="{{ url('audio') }}">
       <h6 class="text-end mt-3"><u>Info Lainnya <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
           </svg></u></h6>
