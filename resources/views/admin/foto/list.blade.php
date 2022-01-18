beb2c8d8e29610a0569ba9579a2bc70e60253ce6
isnaynitika <62254365+isnaynitika@users.noreply.github.com>
2022-01-18 19:00:33 +0700

    Update list.blade.php

--- resources/views/admin/foto/list.blade.php
+++ resources/views/admin/foto/list.blade.php
@@ -14,14 +14,14 @@ Foto Submission
 @endif
 
 <a href="/admin/add-foto" class="btn btn-primary mb-3">Tambah Foto</a>
-<table class="table">
+<table class="table" id="tableFoto">
     <thead class="thead-light">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Tanggal</th>
             <th scope="col">Judul</th>
             <th scope="col">Konten</th>
-            <th scope="col">Caption</th>
+            {{-- <th scope="col">Caption</th> --}}
             <th scope="col">Aksi</th>
         </tr>
     </thead>
@@ -33,7 +33,7 @@ Foto Submission
             <td>{{$foto->judul}}</td>
             <!-- <td>{{Str::limit($foto->konten, 60)}}</td> -->
             <td>{{$foto->konten}}</td>
-            <td>{{$foto->caption}}</td>
+            {{-- <td>{{$foto->caption}}</td> --}}
             <td>
                 <form action="/admin/foto/{{$foto->id}}" method="POST">
                     <a href="/admin/foto/{{$foto->id}}" class="btn btn-info">Edit</a>
@@ -51,4 +51,12 @@ Foto Submission
         @endforelse
     </tbody>
 </table>
+@endsection
+
+@section('table')
+    <script>
+      $(document).ready(function() {
+          $('#tableFoto').DataTable(); //sesuikan id tabel yg dibat
+      } );
+    </script>
 @endsection
